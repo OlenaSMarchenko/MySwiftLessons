05d0b37ba8d4b8360cc5c4df6ff95213c03b4cbc
Olena <olena@Olenas-iMac.local>
2019-01-30 12:29:58 -0800

    Add comments

--- NumbersOfChars.playground/Contents.swift
+++ NumbersOfChars.playground/Contents.swift
@@ -5,21 +5,21 @@ var str = "Hello, playground"
 //H => 1
 //e => 1
 //l => 3
-
+// use dictionary
 func numberOfChar(str: String) {
-    var ourDict: [Character: Int] = [:]
+    var ourDict: [Character: Int] = [:] // create empty dictionary (чтоб накопить буквы)
     
-    for ch in str {
-        if let numberOfChar = ourDict[ch] {
-            ourDict[ch] = numberOfChar + 1
+    for ch in str { //пройдемся по строке
+        if let numberOfChar = ourDict[ch] { // create new variable
+            ourDict[ch] = numberOfChar + 1 // if we have ch - add 1
         } else {
-            ourDict[ch] = 1
+            ourDict[ch] = 1 // created and complete dictionary
         }
         
     }
     
-    for (key, value) in ourDict {
-        guard value == 1 else { continue }
+    for (key, value) in ourDict { //проходим по новому Dictionary
+        guard value == 1 else { continue } // Dictionary = optional
         print("\(key)")
     }
 }
